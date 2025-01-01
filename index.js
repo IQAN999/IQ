84a283279e799d01550de173e048bf11f3d5a458
IQAN999 <01bdfn@gmail.com>
2025-01-01 13:33:14 -0400

    fixing

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const modules = [
   { path: "./features/general/pearlrefill/pearlrefill" },
 
   // Kuudra
+  { path: "./features/kuudra/supplytimes/supplytimes" },
   { path: "./features/kuudra/supplywaypoints/supplywaypoints" },
   { path: "./features/kuudra/pillewaypoints/pillewaypoints" },
   { path: "./features/kuudra/pearlwaypoints/pearlwaypoints" },
